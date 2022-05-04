1f41e70f1844c3b5c06169c5b7fcabb2323b0c8d
Ander Torne <aatorne@outlook.com>
2022-05-03 20:27:23 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:instagram_clone_flutter/providers/user_provider.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_clone_flutter/screens/login_screen.dart';
-import 'package:instagram_clone_flutter/screens/signup_screen.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
-import 'package:instagram_clone_flutter/utils/dimentions.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise app based on platform- web or mobile
   if (kIsWeb) {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyDo8htWle8Mh8YfWdmZEjNNFaDfMshJ1zw',
-            appId: '1:336879357798:web:1bee24fad8be7f5f5982a7',
-            messagingSenderId: '336879357798',
-            projectId: 'insta-flutter-658ab',
-            storageBucket: 'insta-flutter-658ab.appspot.com'
-        ),
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCZ-xrXqD5D19Snauto-Fx_nLD7PLrBXGM",
+        appId: "1:585119731880:web:eca6e4b3c42a755cee329d",
+        messagingSenderId: "585119731880",
+        projectId: "instagram-clone-4cea4",
+        storageBucket: 'instagram-clone-4cea4.appspot.com'
+      ),
     );
   } else {
     await Firebase.initializeApp();
@@ -32,40 +33,46 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark()
-          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      // home: const ResponsiveLayout(
-      //   mobileScreenLayout: MobileScreenLayout(),
-      //   webScreenLayout: WebScreenLayout(),
-      // ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(), //cuando de logea o sale, userchanges, idtokencha
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.active){
-            if(snapshot.hasData){
-              return const ResponsiveLayout(
-                mobileScreenLayout: MobileScreenLayout(),
-                webScreenLayout: WebScreenLayout(),
-              );
-            }else if(snapshot.hasError){
-              return Center(child: Text('${snapshot.error}'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
             }
-          }
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return const Center(
-              child: CircularProgressIndicator(
-                color: primaryColor),
-            );
-          }
-          return const LoginScreen();
-        },
+
+            return const LoginScreen();
+          },
+        ),
       ),
     );
   }
